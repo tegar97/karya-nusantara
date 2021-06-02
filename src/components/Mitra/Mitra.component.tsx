c5b20a1db774a95df83202d82fc3f506aba0da8e
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-02 17:44:16 +0700

    mitra responsive slider

--- src/components/Mitra/Mitra.component.tsx
+++ src/components/Mitra/Mitra.component.tsx
@@ -22,31 +22,25 @@ function OurMitra() {
   }
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
-  var settings = {
+  const settings = {
     dots: true,
-    className: "center",
-
     infinite: false,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
-        className: "center",
-        centerPadding: "60px",
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         },
       },
       {
         breakpoint: 600,
-        className: "center",
-        centerPadding: "60px",
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -55,8 +49,6 @@ function OurMitra() {
       },
       {
         breakpoint: 480,
-        className: "center",
-        centerPadding: "60px",
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -66,13 +58,13 @@ function OurMitra() {
   };
   console.log(data);
   return (
-    <div className="px-5 pt-2 lg:p-20 ">
+    <div className="px-5 pt-2 mb-10 lg:p-20 ">
       <div className="mb-5">
         <HeadingSecondary className="text-center text-blue-100">
           Cerita Dari Mitra Karya Nusantara
         </HeadingSecondary>
       </div>
-      <div className="grid grid-cols-2 gap-3 lg:gap-5 lg:grid-cols-4 lg:mt-20">
+      <div>
         {!data ? (
           <SkeletonTheme color="#fffff" highlightColor="#ffff">
             <p>
@@ -80,23 +72,29 @@ function OurMitra() {
             </p>
           </SkeletonTheme>
         ) : (
-          data.data.map((data) => (
-            <Link href={`/mitra/${data.slug}`}>
-              <div className="relative flex content-end px-5 border-2 border-blue-100 shadow-md cursor-pointer group hover:border-4 hover:shadow-lg hover:translate-y-20">
-                <img
-                  className="w-full max-h-92 "
-                  src={`${process.env.API_LARAVEL}/storage/${data.photoMitra}`}
-                  alt="Mitra 1"
-                />
-                <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-full duration-500 bg-gray-100 opacity-0 transation group-hover:opacity-75">
-                  &nbsp;
+          <div>
+            <Slider {...settings}>
+              {data.data.map((data) => (
+                <div className="p-2">
+                  <Link href={`/mitra/${data.slug}`}>
+                    <div className="relative flex content-end w-full mb-5 border-2 border-blue-100 shadow-md cursor-pointer lg:py-0 lg-py-5 group hover:border-4 hover:shadow-lg hover:translate-y-20">
+                      <img
+                        className="w-full h-80 "
+                        src={`https://api.karyanusantara.co.id/storage/20210602065832-settings-1.png-.jpg`}
+                        alt="Mitra 1"
+                      />
+                      <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-full duration-500 bg-gray-100 opacity-0 transation group-hover:opacity-75">
+                        &nbsp;
+                      </div>
+                      <div className="absolute left-0 w-full p-2 text-center transition duration-500 bg-blue-100 opacity-0 group-hover:opacity-80 bottom-2 ">
+                        <span>{data.ukmName}</span>
+                      </div>
+                    </div>
+                  </Link>
                 </div>
-                <div className="absolute left-0 w-full p-2 text-center transition duration-500 bg-blue-100 opacity-0 group-hover:opacity-80 bottom-2 ">
-                  <span>{data.ukmName}</span>
-                </div>
-              </div>
-            </Link>
-          ))
+              ))}
+            </Slider>
+          </div>
         )}
       </div>
     </div>
